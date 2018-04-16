86f8c44b178047b3d4f75d5a0bd28c4b505f2179
Fulwin <vinty_email@163.com>
2018-04-16 18:05:01 +0800

    Building user manage pages

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,5 @@ Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail'
 Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
 Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 
+// User Manage Routes...
+Route::resource('users', 'UsersController');
